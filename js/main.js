7de4013e78c53ff8eeeadd9269031bd077ee5213
ricvolpe <volpatorv@gmail.com>
2020-11-17 20:31:58 +0000

    Adds Matomo

--- js/main.js
+++ js/main.js
@@ -38,3 +38,16 @@ function switchLigth() {
     }
     localStorage.setItem('lastTimeStamp', new Date().getTime());
 }
+
+// Matomo code
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+   var u="https://analytics.volpato.io/";
+   _paq.push(['setTrackerUrl', u+'matomo.php']);
+   _paq.push(['setSiteId', '1']);
+   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+   g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
