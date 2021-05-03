999d94786c838184f297ec38cfdb6e9c569b0ce4
ricvolpe <volpatorv@gmail.com>
2021-05-03 11:33:16 +0100

    Built header;

---
+++ js/header.js
@@ -0,0 +1,15 @@
+document.write(`
+    <div>
+    <div class="site-header">
+    <h1 style="float: left">Riccardo Volpato</h1>
+    <div class="terminal-nav">
+        <nav class="terminal-menu">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li></li><a id="ligthSwitch" onclick="switchLigth()">ligthSwitch</a></li>
+            </ul>
+        </nav>
+    </div>
+    </div>
+    </div>
+`);
