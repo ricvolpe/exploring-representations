9ab39dacbe4179db98ef4795c4a76c6a88300ec6
Riccardo Volpato <rvolpato@twitter.com>
2022-07-19 13:52:09 +0100

    Added poems

--- js/header_top.js
+++ js/header_top.js
@@ -5,6 +5,7 @@ document.write(`
                 <ul>
                     <li><a href="index.html">Home</a></li>
                     <li><a href="articles.html">Articles</a></li>
+                    <li><a href="poems.html">Poems</a></li>
                     <li></li><a id="ligthSwitch" onclick="switchLigth()">ligthSwitch</a></li>
                 </ul>
             </nav>
